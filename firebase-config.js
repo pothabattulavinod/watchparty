c32fab3a59322ea14c58a288a251e9dfaafad3d1
Pothabattula Vinod <83913882+pothabattulavinod@users.noreply.github.com>
2025-09-28 09:51:50 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKvIVVVSwNTSIFU66RUOUGAJT0v896Mho",
+  authDomain: "watch-party-bd392.firebaseapp.com",
+  databaseURL: "https://watch-party-bd392-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "watch-party-bd392",
+  storageBucket: "watch-party-bd392.firebasestorage.app",
+  messagingSenderId: "583748650951",
+  appId: "1:583748650951:web:c7fcccd1486f173d437be2"
+};
+
+
+
+
+
